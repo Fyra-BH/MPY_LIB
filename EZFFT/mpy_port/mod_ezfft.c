c118eb9752a1c615b5e7f761e6aec0a3f0761316
Fyra-BH <2472708282@qq.com>
2021-03-04 22:13:45 +0800

    修复一bug

--- EZFFT/mpy_port/mod_ezfft.c
+++ EZFFT/mpy_port/mod_ezfft.c
@@ -25,7 +25,8 @@ STATIC mp_obj_t ez_fftN(mp_obj_t buffer)
         printf("please input float array");
         return mp_obj_new_bool(0);
     }
-    return mp_obj_new_bool(!fft_N(array->len, (float*)(array->items)));
+    //由于复数列buffer是实部虚部交错的，所以len要除以2
+    return mp_obj_new_bool(!fft_N(array->len / 2, (float*)(array->items)));
 }
 
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(ez_fft_fftN_obj, ez_fftN);

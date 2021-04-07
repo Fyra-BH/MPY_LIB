3b3951695471276e08250d5cc40cd08a05889604
Fyra-BH <2472708282@qq.com>
2021-04-07 19:31:07 +0800

    -

--- powerADDA/micropython.mk
+++ powerADDA/micropython.mk
@@ -4,7 +4,6 @@ EXAMPLE_MOD_DIR := $(USERMOD_DIR)
 SRC_USERMOD += $(EXAMPLE_MOD_DIR)/stm32_port/drv_timer.c
 SRC_USERMOD += $(EXAMPLE_MOD_DIR)/stm32_port/drv_dac.c
 SRC_USERMOD += $(EXAMPLE_MOD_DIR)/mpy_port/mod_powerADDA.c
-SRC_USERMOD += $(EXAMPLE_MOD_DIR)/../../lib/stm32lib/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dac.c
 
 # We can add our module folder to include paths if needed
 # This is not actually needed in this example.

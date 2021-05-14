9ff0d26258a9143d4c3ca69bc59c0d1cc48e3dd2
Fyra-BH <2472708282@qq.com>
2021-05-14 20:27:29 +0800

    backup

--- EZFFT/ezfft.c
+++ EZFFT/ezfft.c
@@ -184,7 +184,7 @@ int fft_N(int N, float* buff)
 int ifft_N(int N, float* buff)
 {
     if(ezlog2(N) == 0 ) return 1;
-    complex_t* buff_c = (complex_t*)malloc(sizeof(complex_t) * N);
+    complex_t* buff_c = (complex_t*)ezfft_malloc(sizeof(complex_t) * N);
     //将原序列的序号比特翻转，并存入临时数组
     for (size_t i = 0; i < N; i++)
     {
@@ -192,7 +192,7 @@ int ifft_N(int N, float* buff)
         buff_c[i].im = *(buff + 2 * bit_reverse(i, ezlog2(N)) + 1);
     }
     //计算旋转因子
-    complex_t *w = (complex_t *)malloc(sizeof(complex_t) * N / 2);
+    complex_t *w = (complex_t *)ezfft_malloc(sizeof(complex_t) * N / 2);
     for (int i = 0; i < N / 2; i++)
         *(w + i) = W(-N, i);    //注意因为是IFFT,这里的参数是-N
     //蝶形运算
@@ -215,8 +215,8 @@ int ifft_N(int N, float* buff)
         *(buff + i * 2) = buff_c[i].re / (float)N;      //注意因为是IFFT,这里要除以N,下同
         *(buff + i * 2 + 1) = buff_c[i].im / (float)N;
     }
-    free(buff_c);
-    free(w);
+    ezfft_free(buff_c);
+    ezfft_free(w);
     return 0;
 }
 
